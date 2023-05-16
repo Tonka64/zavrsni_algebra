76d2bf163c27a3ceb6c275163fd0e9e6381362fc
Tonka64 <zidov.tonka@gmail.com>
2023-05-16 22:24:20 +0200

    header i about

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -6,27 +6,29 @@ const Login = (props) =>{
     const [usernameText, setUsernameText] = useState("");
     const navigate = useNavigate();
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e)=>{
         e.preventDefault();
         props.onLogin(usernameText);
-        navigate("/");
+        navigate("/");        
     }
-
-    return <div className="container">
+    
+    return (
+      
+    <div className="container">
         <form className="login-form" onSubmit={handleSubmit}>
-            <label for="title">
+            <label htmlFor="title">
                 <h1>Login</h1>
             </label>
-            <input
+            <input 
             id="title"
-            type="text"
-            placeholder="Enter your username"
-            required
-            value={usernameText}
-            onChange={(e) => setUsernameText(e.target.value)}/>
+            type="text" 
+            placeholder="Enter your username" 
+            required 
+            value={usernameText} 
+            onChange={(e)=> setUsernameText(e.target.value)}/>
             <button className="login-btn">Login</button>
         </form>
-    </div>
+    </div>)
 }
 
 export default Login;
