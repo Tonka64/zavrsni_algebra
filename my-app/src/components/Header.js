d04e3ab8884e175bcdc93304bf36d39129f9cebf
Tonka64 <zidov.tonka@gmail.com>
2023-05-16 22:47:25 +0200

    uredila logout-btn

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -7,11 +7,13 @@ const Header = (props) =>{
         {isLoggedIn && 
         <div className="App-header">
         <button className='sidebar-btn' onClick={props.toggleSidebar}>Sidebar</button>
-        <h1>{props.username}'s Chat Room</h1>
-        <button onClick={props.handleLogout}>Logout</button>
+        <h1 className="nazivSobe">{props.username}'s Chat Room</h1>
+        <div className="logout-btn"> 
+        <button className="logout-btn2" onClick={props.handleLogout}>Logout</button>
+        </div>
       </div>}
 
-      {!isLoggedIn &&  <div className="App-header">
+      {!isLoggedIn &&  <div /*className="App-header"*/>
         <div></div>
         <h1 className="header2">Chat Application</h1>
         {/* Prazan div služi za flex pozicioniranje elemenata */}
